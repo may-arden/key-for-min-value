ac1783584416440cf862fae3c9ffd7865195b79d
may arden <mayard3n@gmail.com>
2020-10-28 16:49:54 -0700

    commit tree ac6751a26f549e7d1695139f1ccece5248e5bd6f

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,25 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
 
+
+def key_for_min_value(name_hash)
+  if name_hash.empty?
+    return nil
+  end
+    min = 0
+    item = ""
+    name_hash.each do |key, value|
+  if min == 0 
+    min = value
+    item = key
+  elsif value < min
+    min = value
+    item = key
+    end
 end
+    item
+end
+
+
+
